44273fad2a0e5746a2b51a63576a6f033573d5d9
hibs <hibs@circuitco.de>
2018-04-29 17:01:32 +0100

    regex for understanding lines, skip empty lines (probably a bad idea), not transmitting time yet

--- line.go
+++ line.go
@@ -1,11 +1,13 @@
 package logs
 
 import (
+	"errors"
 	"io"
 	"time"
 	"bufio"
 	"fmt"
 	"os"
+	"regexp"
 )
 
 type LineParser struct {
@@ -21,6 +23,24 @@ type Line struct {
 	Nick string
 	Message string
 	Channel string
+}
+
+// Common regex fields
+var IRCTIME = `[0-9]{2}:[0-9]{2}:[0-9]{2}`
+var IRCNICK = `[\.a-zA-Z0-9_\-\\\[\]\{\}\^\'\|\x60~]+`
+var GREEDY = `.*`
+
+// Skip joins and parts
+var skiplist []*regexp.Regexp = []*regexp.Regexp {
+	regexp.MustCompile(fmt.Sprintf(`\[(?P<time>%s)\] \*\*\* \w+: (?P<nick>%s) (?P<msg>%s)`, IRCTIME, IRCNICK, GREEDY)),
+}
+
+// Pick up messages, mode changes, nick changes and notices
+var re []*regexp.Regexp = []*regexp.Regexp {
+	regexp.MustCompile(fmt.Sprintf(`\[(?P<time>%s)\] -(?P<nick>%s)- (?P<msg>%s)`, IRCTIME, IRCNICK, GREEDY)),
+	regexp.MustCompile(fmt.Sprintf(`\[(?P<time>%s)\] \* (?P<nick>%s) (?P<msg>%s)`, IRCTIME, IRCNICK, GREEDY)),
+	regexp.MustCompile(fmt.Sprintf(`\[(?P<time>%s)\] \*\*\* (?P<nick>%s) (?P<msg>%s)`, IRCTIME, IRCNICK, GREEDY)),
+	regexp.MustCompile(fmt.Sprintf(`\[(?P<time>%s)\] <(?P<nick>%s)> (?P<msg>%s)`, IRCTIME, IRCNICK, GREEDY)),
 }
 
 func (p *LineParser) ParseLinesForever() {
@@ -40,15 +60,50 @@ func (p *LineParser) ParseLinesForFile(file Logfile) {
 	rdr := bufio.NewReader(f)
 	index := int64(0)
 	for line := ""; e == nil; line, e = rdr.ReadString('\n') {
-		p.Out <- p.ParseLine(&file, &line, index)
+		if line == "" {
+			continue
+		}
+		result, e := p.ParseLine(&file, &line, index)
+		if e == nil {
+			//p.Out <- result
+			fmt.Sprintf("%s", result)
+		}
 		index += int64(len(line))
 	}
 	if e != nil && e != io.EOF {
 		fmt.Println("Error: " + e.Error())
+		panic("Unexpected error")
 	}
 }
 
-func (p *LineParser) ParseLine(file *Logfile, line *string, index int64) Line {
-	fmt.Printf("Found line %d with ind %d\n", len(*line), index)
-	return Line{}
+func (p *LineParser) ParseLine(file *Logfile, line *string, index int64) (l Line, e error) {
+	for _, r := range skiplist {
+			m := r.FindAllString(*line, -1)
+			if len(m) > 0 {
+				e = errors.New("skip")
+				return
+			}
+	}
+	for _, r := range re {
+			// test regexp against line
+			match := r.FindStringSubmatch(*line)
+			if len(match) > 0 {
+				sub := r.SubexpNames()
+				for i, name := range sub {
+					if i == 0 { continue }
+					switch name {
+						case "nick":
+							l.Nick = sub[i]
+						case "time":
+//							l.Time = sub[i]
+						case "msg":
+							l.Message = sub[i]
+						default:
+							panic("Don't understand " + name)
+					}
+				}
+				return
+			}
+	}
+	panic("NO MATCH")
 }
